94d4114481e5fb34c67d6cb70a6be3d60e91d924
Miguel Dias <miguel.a.r.diass@gmail.com>
2023-03-01 22:35:27 +0100

    try

--- udagram/udagram-api/src/aws.ts
+++ udagram/udagram-api/src/aws.ts
@@ -3,10 +3,17 @@ import { config } from "./config/config";
 
 //Credentials are auto set according to the documentation https://docs.aws.amazon.com/sdk-for-javascript/v2/developer-guide/setting-credentials-node.html and the default profile is "Default anyway"
 
+const credentials = new AWS.Credentials({
+  accessKeyId: 'ASIA4OWU7IX3PRENGUNP',
+  secretAccessKey: 'vVoZ/NNO7kSsyQpra6hGk57cKTpIEE3JDd082Ta4',
+  sessionToken: 'FwoGZXIvYXdzEP3//////////wEaDJT+WYuCYPcAvZvYaCLVARLdnpIhwNnGl1Y9NvOlJzIxK0uycmYpzyaRYcggPxDibGsnIsN+cUH9yPHlFFjZ3e2CLoKrn1UTkFsqybSirmsw0gXxrHcDGiugyfZhSYZ23RmL4AQbFO7YNQ4ZeL4VuyDfBuEJukGigROS2WjlOxVWRZV71Sxp00CiC7pVJEC7iShZG+xcml5laTF5h7Rnyw4U87Z7U9oKvjhTS0c/5Q8xjkcFL6FzzM/EaOZldDI52BzN/pNk/KBJnBYX4exOtT141WMr18MsgjPEJZ88MIZ39Xmt4Siky/6fBjItaC/KjHOrl6tFEg32dpqZJygi8V46jiQ2/M8yrhKrBCoHUC+ZTcAMK7BToJhZ'
+})
+
 export const s3 = new AWS.S3({
   signatureVersion: "v4",
   region: config.aws_region,
   params: { Bucket: config.aws_media_bucket },
+  credentials: credentials
 });
 
 // Generates an AWS signed URL for retrieving objects
