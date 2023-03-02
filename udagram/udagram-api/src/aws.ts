b97f7f13d6c321867800d400cb3cacccfdefab8b
Miguel Dias <miguel.a.r.diass@gmail.com>
2023-03-02 09:40:14 +0100

    add without credentials

--- udagram/udagram-api/src/aws.ts
+++ udagram/udagram-api/src/aws.ts
@@ -8,6 +8,8 @@ const credentials = new AWS.Credentials({
   secretAccessKey: config.aws_secret
 })
 
+AWS.config.credentials = credentials;
+
 export const s3 = new AWS.S3({
   signatureVersion: "v4",
   region: config.aws_region,
